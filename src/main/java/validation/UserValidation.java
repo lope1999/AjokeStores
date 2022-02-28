bad5099e166a1a5cc0fcef5f5e7707230b231296
saviobosco <Johnboscoomebe@yahoo.com>
2022-02-28 13:50:14 +0100

    Added the user registration business logic

---
+++ src/main/java/validation/UserValidation.java
@@ -0,0 +1,64 @@
+package validation;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+public class UserValidation implements Serializable {
+
+    @NotEmpty(message = "First name can not be empty")
+    private String firstName;
+
+    @NotEmpty(message = "Last name can not be empty")
+    private String lastName;
+
+    @NotEmpty(message = "username can not be empty")
+    private String username;
+
+    @NotEmpty(message = "Email can not be empty")
+    @Email(message = "Please provide a valid email id")
+    private String email;
+
+    @NotEmpty(message = "Password can not be empty")
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
